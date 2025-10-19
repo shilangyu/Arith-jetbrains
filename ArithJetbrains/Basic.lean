155229049a8f183c828296c7b94f5aef2bf5919c
shilangyu <xmarcinmarcin@gmail.com>
2025-10-19 14:42:02 +0200

    op: merge all op rules
    This will simplify greatly the small step semantics.

--- ArithJetbrains/Basic.lean
+++ ArithJetbrains/Basic.lean
@@ -1,9 +1,12 @@
+inductive Op where
+  | add
+  | sub
+  | mul
+  | div
+
 inductive Term where
   | const : Nat -> Term
-  | add : Term → Term → Term
-  | sub : Term → Term → Term
-  | mul : Term → Term → Term
-  | div : Term → Term → Term
+  | op : Op -> Term → Term → Term
 
 inductive Result (α : Type) where
   | ok : α → Result α
@@ -16,19 +19,15 @@ instance : Monad Result where
     | Result.divByZero, _ => Result.divByZero
 
 def eval : Term → Result Nat
-  | Term.const n => pure n
-  | Term.add t1 t2 => eval t1 >>= fun r1 =>
-                      eval t2 >>= fun r2 =>
-                      pure (r1 + r2)
-  | Term.sub t1 t2 => eval t1 >>= fun r1 =>
-                      eval t2 >>= fun r2 =>
-                      pure (r1 - r2)
-  | Term.mul t1 t2 => eval t1 >>= fun r1 =>
-                      eval t2 >>= fun r2 =>
-                      pure (r1 * r2)
-  | Term.div t1 t2 => eval t1 >>= fun r1 =>
-                      eval t2 >>= fun r2 =>
-                      if r2 == 0 then Result.divByZero else Result.ok (r1 / r2)
+  | .const n => pure n
+  | .op op t1 t2 =>
+      eval t1 >>= fun r1 =>
+      eval t2 >>= fun r2 =>
+      match op with
+      | .add => pure (r1 + r2)
+      | .sub => pure (r1 - r2)
+      | .mul => pure (r1 * r2)
+      | .div => if r2 == 0 then Result.divByZero else pure (r1 / r2)
 
-#eval eval (Term.add (Term.const 5) (Term.mul (Term.const 2) (Term.const 3))) -- 11
-#eval eval (Term.div (Term.const 10) (Term.sub (Term.const 5) (Term.const 5))) -- divByZero
+#eval eval (Term.op .add (Term.const 5) (Term.op .mul (Term.const 2) (Term.const 3))) -- 11
+#eval eval (Term.op .div (Term.const 10) (Term.op .sub (Term.const 5) (Term.const 5))) -- divByZero
