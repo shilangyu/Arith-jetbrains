8b628af5ae0f3fd7a076908e1667a0e7a530374d
shilangyu <xmarcinmarcin@gmail.com>
2025-10-19 16:20:12 +0200

    task: add variable references

--- ArithJetbrains/Basic.lean
+++ ArithJetbrains/Basic.lean
@@ -6,11 +6,13 @@ inductive Op where
 
 inductive Term where
   | const : Nat -> Term
+  | var : String -> Term
   | op : Op -> Term → Term → Term
 
 inductive Result (α : Type) where
   | ok : α → Result α
   | divByZero : Result α
+  | unknownReference : String -> Result α
 
 @[simp]
 instance : Monad Result where
@@ -18,24 +20,34 @@ instance : Monad Result where
   bind
     | Result.ok n, f => f n
     | Result.divByZero, _ => Result.divByZero
+    | Result.unknownReference s, _ => Result.unknownReference s
+
+def Context := String → Option Nat
 
 @[simp]
-def eval : Term → Result Nat
+def eval (ctx : Context) : Term → Result Nat
   | .const n => pure n
+  | .var s =>
+      match ctx s with
+      | some n => pure n
+      | none => Result.unknownReference s
   | .op op t1 t2 =>
-      eval t1 >>= fun r1 =>
-      eval t2 >>= fun r2 =>
+      eval ctx t1 >>= fun r1 =>
+      eval ctx t2 >>= fun r2 =>
       match op with
       | .add => pure (r1 + r2)
       | .sub => pure (r1 - r2)
       | .mul => pure (r1 * r2)
       | .div => if r2 == 0 then Result.divByZero else pure (r1 / r2)
 
-#eval eval (Term.op .add (Term.const 5) (Term.op .mul (Term.const 2) (Term.const 3))) -- 11
-#eval eval (Term.op .div (Term.const 10) (Term.op .sub (Term.const 5) (Term.const 5))) -- divByZero
+#eval eval (fun _ => none) (Term.op .add (Term.const 5) (Term.op .mul (Term.const 2) (Term.const 3))) -- 11
+#eval eval (fun _ => none) (Term.op .div (Term.const 10) (Term.op .sub (Term.const 5) (Term.const 5))) -- divByZero
 
 
-inductive Step : Term -> Term -> Prop where
+inductive Step {ctx : Context} : Term -> Term -> Prop where
+  | var : ∀ s n,
+      ctx s = some n →
+      Step (Term.var s) (Term.const n)
   | opCtx1 : ∀ op t1 t1' t2,
       Step t1 t1' →
       Step (Term.op op t1 t2) (Term.op op t1' t2)
@@ -53,22 +65,22 @@ inductive Step : Term -> Term -> Prop where
       Step (Term.op .div (.const n1) (.const n2)) (.const (n1 / n2))
 
 -- reflexive transitive closure of Step
-inductive StepStar : Term -> Term -> Prop where
+inductive StepStar {ctx : Context} : Term -> Term -> Prop where
   | refl : ∀ t, StepStar t t
-  | trans : ∀ t1 t2 t3, Step t1 t2 → StepStar t2 t3 → StepStar t1 t3
+  | trans : ∀ t1 t2 t3, @Step ctx t1 t2 → StepStar t2 t3 → StepStar t1 t3
 
 -- reducing a term does not affect its evaluation
-theorem step_eval : ∀ t t',
-    Step t t' →
-    eval t = eval t' := by
+theorem step_eval {ctx : Context} : ∀ t t',
+    @Step ctx t t' →
+    eval ctx t = eval ctx t' := by
     intro t t' h
     induction h <;> simp [*]
 
 -- correctness of the small-step semantics
-theorem step_correct : ∀ t t' n,
-    StepStar t t' →
+theorem step_correct {ctx : Context} : ∀ t t' n,
+    @StepStar ctx t t' →
     t' = Term.const n →
-    eval t = Result.ok n := by
+    eval ctx t = Result.ok n := by
   intro t t' n h ht'
   induction h with
   | refl => intros; subst ht'; rfl
