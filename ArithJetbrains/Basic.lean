e4101e8df4f1a73ad0c780f537141c323a2da0a3
shilangyu <xmarcinmarcin@gmail.com>
2025-10-25 15:51:55 +0200

    Add binders

--- ArithJetbrains/Basic.lean
+++ ArithJetbrains/Basic.lean
@@ -13,6 +13,7 @@ inductive Comp where
 
 inductive Term where
   | const : Nat -> Term
+  | bind : String -> Term -> Term -> Term
   | var : String -> Term
   | cond : Term -> Comp -> Term -> Term -> Term -> Term
   | op : Op -> Term → Term → Term
@@ -31,6 +32,8 @@ instance : Monad Result where
     | Result.unknownReference s, _ => Result.unknownReference s
 
 def Context := String → Option Nat
+def extend (ctx : Context) (s : String) (n : Nat) : Context :=
+  fun key => if key == s then some n else ctx key
 
 @[simp]
 def compare (c : Comp) (n1 n2 : Nat) : Bool :=
@@ -44,6 +47,9 @@ def compare (c : Comp) (n1 n2 : Nat) : Bool :=
 @[simp]
 def eval (ctx : Context) : Term → Result Nat
   | .const n => pure n
+  | .bind s e b =>
+      eval ctx e >>= fun n =>
+      eval (extend ctx s n) b
   | .cond t1 cmp t2 tt te =>
       eval ctx t1 >>= fun r1 =>
       eval ctx t2 >>= fun r2 =>
@@ -68,48 +74,56 @@ def eval (ctx : Context) : Term → Result Nat
 #eval eval (fun _ => none) (Term.op .div (Term.const 10) (Term.op .sub (Term.const 5) (Term.const 5))) -- divByZero
 
 
-inductive Step {ctx : Context} : Term -> Term -> Prop where
-  | var : ∀ s n,
+inductive Step : Context → Term → Term → Prop where
+  | bindCtx : ∀ ctx s e e' b,
+      Step ctx e e' →
+      Step ctx (.bind s e b) (.bind s e' b)
+  | bindBody : ∀ s n b b',
+      Step (extend ctx s n) b b' →
+      Step ctx (.bind s (.const n) b) (.bind s (.const n) b')
+  | bind : ∀ ctx s n b,
+      Step ctx (.bind s (.const n) (.const b)) (.const b)
+  | var : ∀ ctx s n,
       ctx s = some n →
-      Step (Term.var s) (Term.const n)
-  | condCtx1 : ∀ t1 t1' cmp t2 tt te,
-      Step t1 t1' →
-      Step (Term.cond t1 cmp t2 tt te) (Term.cond t1' cmp t2 tt te)
-  | condCtx2 : ∀ n1 t2 t2' cmp tt te,
-      Step t2 t2' →
-      Step (Term.cond (.const n1) cmp t2 tt te) (Term.cond (.const n1) cmp t2' tt te)
-  | condTrue : ∀ n1 n2 tt te cmp,
+      Step ctx (.var s) (.const n)
+  | condCtx1 : ∀ ctx t1 t1' cmp t2 tt te,
+      Step ctx t1 t1' →
+      Step ctx (.cond t1 cmp t2 tt te) (.cond t1' cmp t2 tt te)
+  | condCtx2 : ∀ ctx n1 t2 t2' cmp tt te,
+      Step ctx t2 t2' →
+      Step ctx (.cond (.const n1) cmp t2 tt te) (.cond (.const n1) cmp t2' tt te)
+  | condTrue : ∀ ctx n1 n2 tt te cmp,
       compare cmp n1 n2 = true →
-      Step (Term.cond (.const n1) cmp (.const n2) tt te) tt
-  | condFalse : ∀ n1 n2 tt te cmp,
+      Step ctx (.cond (.const n1) cmp (.const n2) tt te) tt
+  | condFalse : ∀ ctx n1 n2 tt te cmp,
       compare cmp n1 n2 = false →
-      Step (Term.cond (.const n1) cmp (.const n2) tt te) te
-  | opCtx1 : ∀ op t1 t1' t2,
-      Step t1 t1' →
-      Step (Term.op op t1 t2) (Term.op op t1' t2)
-  | opCtx2 : ∀ op n1 t2 t2',
-      Step t2 t2' →
-      Step (Term.op op (.const n1) t2) (Term.op op (.const n1) t2')
-  | addConst : ∀ n1 n2,
-      Step (Term.op .add (.const n1) (.const n2)) (.const (n1 + n2))
-  | subConst : ∀ n1 n2,
-      Step (Term.op .sub (.const n1) (.const n2)) (.const (n1 - n2))
-  | mulConst : ∀ n1 n2,
-      Step (Term.op .mul (.const n1) (.const n2)) (.const (n1 * n2))
-  | divConst : ∀ n1 n2,
+      Step ctx (.cond (.const n1) cmp (.const n2) tt te) te
+  | opCtx1 : ∀ ctx op t1 t1' t2,
+      Step ctx t1 t1' →
+      Step ctx (.op op t1 t2) (.op op t1' t2)
+  | opCtx2 : ∀ ctx op n1 t2 t2',
+      Step ctx t2 t2' →
+      Step ctx (.op op (.const n1) t2) (.op op (.const n1) t2')
+  | addConst : ∀ ctx n1 n2,
+      Step ctx (.op .add (.const n1) (.const n2)) (.const (n1 + n2))
+  | subConst : ∀ ctx n1 n2,
+      Step ctx (.op .sub (.const n1) (.const n2)) (.const (n1 - n2))
+  | mulConst : ∀ ctx n1 n2,
+      Step ctx (.op .mul (.const n1) (.const n2)) (.const (n1 * n2))
+  | divConst : ∀ ctx n1 n2,
       n2 ≠ 0 →
-      Step (Term.op .div (.const n1) (.const n2)) (.const (n1 / n2))
+      Step ctx (.op .div (.const n1) (.const n2)) (.const (n1 / n2))
 
 -- reflexive transitive closure of Step
-inductive StepStar {ctx : Context} : Term -> Term -> Prop where
-  | refl : ∀ t, StepStar t t
-  | trans : ∀ t1 t2 t3, @Step ctx t1 t2 → StepStar t2 t3 → StepStar t1 t3
+inductive StepStar : Context → Term → Term → Prop where
+  | refl : ∀ ctx t, StepStar ctx t t
+  | trans : ∀ ctx t1 t2 t3, Step ctx t1 t2 → StepStar ctx t2 t3 → StepStar ctx t1 t3
 
 -- reducing a term does not affect its evaluation
-theorem step_eval {ctx : Context} : ∀ t t',
-    @Step ctx t t' →
+theorem step_eval : ∀ ctx t t',
+    Step ctx t t' →
     eval ctx t = eval ctx t' := by
-    intro t t' h
+    intro ctx t t' h
     induction h <;> simp [*]
     case condTrue hcmp =>
       simp at hcmp
@@ -121,12 +135,12 @@ theorem step_eval {ctx : Context} : ∀ t t',
       intro; contradiction
 
 -- correctness of the small-step semantics
-theorem step_correct {ctx : Context} : ∀ t t' n,
-    @StepStar ctx t t' →
+theorem step_correct : ∀ ctx t t' n,
+    StepStar ctx t t' →
     t' = Term.const n →
     eval ctx t = Result.ok n := by
-  intro t t' n h ht'
+  intro ctx t t' n h ht'
   induction h with
   | refl => intros; subst ht'; rfl
   | trans t1 t2 t3 hstep hstar ih =>
-      rw [step_eval _ _ hstep, ih ht']
+      rw [step_eval _ _ _ hstep, ih ht']
